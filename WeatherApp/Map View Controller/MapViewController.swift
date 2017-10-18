13449fa0e5286602d0b0a9e1bf2aee2faa4cdc0d
Peter Stajger <stajger.p@gmail.com>
2017-10-18 15:33:46 +0200

    added core location manager and update user's location, handle permissions

--- WeatherApp/Map View Controller/MapViewController.swift
+++ WeatherApp/Map View Controller/MapViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
     
     lazy var tapGestureRecognizer: UITapGestureRecognizer = {
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(hello(sender:)))
@@ -23,19 +26,26 @@ class MapViewViewController: UIViewController {
     
     override func loadView() {
         view = MKMapView()
-        mapView.showsCompass = true
-        mapView.showsUserLocation = true
+        mapView.delegate = self
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         view.addGestureRecognizer(tapGestureRecognizer)
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
     
     // MARK: Private Methods
@@ -48,6 +58,39 @@ class MapViewViewController: UIViewController {
         default:
             break
         }
+    }
+    
+}
+
+extension MapViewViewController : MKMapViewDelegate {
+    
+    public func mapView(_ mapView: MKMapView, didFailToLocateUserWithError error: Error) {
+        let alert = UIAlertController.okAlert(title: nil, message: "There was an error when locating user")
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension MapViewViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            let alert = UIAlertController.okAlert(title: "Location Services", message: "To show user's location please enable location services in Settings app.")
+            present(alert, animated: true, completion: nil)
+        default: break
+        }
+    }
+}
+
+extension UIAlertController {
+    
+    static func okAlert(title: String?, message: String?, okHandler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: okHandler))
+        return alert
     }
     
 }
