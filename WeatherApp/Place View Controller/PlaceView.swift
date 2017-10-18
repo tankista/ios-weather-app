448ab6e54468577fc850a0dd0e03e69dfa21e698
Peter Stajger <stajger.p@gmail.com>
2017-10-18 17:49:36 +0200

    added basic view that shows weather data

---
+++ WeatherApp/Place View Controller/PlaceView.swift
@@ -0,0 +1,19 @@
+//
+//  PlaceView.swift
+//  WeatherApp
+//
+//  Created by Peter Stajger on 18/10/2017.
+//  Copyright © 2017 Peter Stajger. All rights reserved.
+//
+
+import UIKit
+
+final class PlaceView : UIView {
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var himidityLabel: UILabel!
+    @IBOutlet weak var visiblityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+}
